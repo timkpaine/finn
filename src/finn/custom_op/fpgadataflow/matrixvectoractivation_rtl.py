89ae04103d37e3a51f2411a443bd093761ce4b56
mmrahorovic <mmrahorovic@hotmail.com>
2023-12-17 13:07:10 +0000

    minor fix

--- src/finn/custom_op/fpgadataflow/matrixvectoractivation_rtl.py
+++ src/finn/custom_op/fpgadataflow/matrixvectoractivation_rtl.py
@@ -986,8 +986,7 @@ class MatrixVectorActivation_rtl(HLSCustomOp):
         # )
         critical_path_dsps = np.floor((clk - 0.741) / 0.605 + 1)
         max_chain_len = np.ceil(self.get_nodeattr("SIMD") / 3)
-        dsp_chain_len = critical_path_dsps if critical_path_dsps < max_chain_len
-                                              else max_chain_len
+        dsp_chain_len = critical_path_dsps if critical_path_dsps < max_chain_len else max_chain_len
         return dsp_chain_len
         # return 1
 
