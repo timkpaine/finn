730ed40b7bfe4106315288bc49a679d11a0c943b
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2024-05-04 14:23:06 -0400

    .

--- install.sh
+++ install.sh
@@ -6,3 +6,10 @@ python -m virtualenv venv
 
 pip install -r requirements-tim.txt
 
+pip install --no-deps -e deps/qonnx/
+sudo rm -rf deps/finn-experimental/src/finn_experimental.egg-info/
+pip install --no-deps -e deps/finn-experimental/
+sudo rm -rf deps/brevitas/src/brevitas.egg-info/
+pip install --no-deps -e deps/brevitas/
+sudo rm -rf deps/pyverilator/PyVerilator.egg-info/
+pip install --no-deps -e deps/pyverilator/
